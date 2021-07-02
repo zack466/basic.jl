ba0058d89c639eeead06413fe1b2b99bf40a0bbe
Zachary Huang <zack466@gmail.com>
2021-07-01 23:08:37 -0400

    beginning new lexer

--- src/lexer.jl
+++ src/lexer.jl
@@ -1,1 +1,9 @@
 # use parser combinators for clean and modular lexing
+
+using CombinedParsers
+
+struct LexOptions
+    # Requires spaces between keywords
+    # True in ANSI BASIC spec, but not Vintage BASIC
+    require_spaces::Boolean 
+end
