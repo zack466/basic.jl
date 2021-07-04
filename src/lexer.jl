545cb364f003fa19750ae11f163d3116004a2d3b
Zachary Huang <zack466@gmail.com>
2021-07-04 19:43:20 -0400

    finish lexer

--- src/lexer.jl
+++ src/lexer.jl
@@ -7,6 +7,7 @@ struct LexOptions
     # Requires spaces between keywords
     # True in ANSI BASIC spec, but not Vintage BASIC
     require_spaces::Bool
+    caseless_keywords::Bool
 end
 
 struct Token
@@ -27,12 +28,13 @@ end
 end
 
 @syntax parseIdentifier = map(!Sequence(
-    Repeat( CharIn('a':'z', 'A':'Z', '0':'9')),
+    Repeat( CharIn('a':'z', 'A':'Z', '0':'9'), min=1),
     Either("\$", "%", "") # type declaration: $ = string, % = integer
 )) do id
     Token(:Identifier, id)
 end
 
+# each keyword corresponds with a symbol :UPPER
 keywords = [
     "INPUT",
     "IF",
@@ -40,9 +42,79 @@ keywords = [
     "PRINT",
     "GOTO",
     "END",
+    "(",
+    ")",
+    ",",
+    ":",
+    ";",
+    "-",
+    "+",
+    "*",
+    "^", # exp
+    "/",
+    "=", # equality test
+    "<>", # inequality test
+    "<=",
+    ">=",
+    "<",
+    ">",
+    "NOT",
+    "AND",
+    "OR",
 ]
 
-@syntax parseKeyword = map(!Either(keywords)) do kw
+# uppercase keywords only, does not require whitespace after keyword
+@syntax parseKeywordSpacelessUpper = map(!Either(keywords)) do kw
     Token(Symbol(kw), nothing)
 end
+# case insensitive keywords, does not require whitespace after keyword
+@syntax parseKeywordSpacelessCaseless = map(
+    !mapreduce(caseless, |, keywords)
+) do kw
+    Token(Symbol(uppercase(kw)), nothing)
+end
 
+# uppercase keywords only, requires whitespace after keyword
+@syntax parseKeywordUpper = map(Sequence(!Either(keywords), PositiveLookahead(re"\s"))) do kw
+    Token(Symbol(kw[1]), nothing)
+end
+
+# case insensitive keywords, requires whitespace after keyword
+@syntax parseKeywordCaseless = map(
+    Sequence(!mapreduce(caseless, |, keywords), PositiveLookahead(re"\s"))
+) do kw
+    Token(Symbol(uppercase(kw[1])), nothing)
+end
+
+@syntax parseToken = map(Sequence(Optional(whitespace_newline), Either(parseNumber, parseString, parseKeywordSpacelessCaseless, parseIdentifier))) do r
+    r[2]
+end
+
+mutable struct Lex
+    source::String
+end
+
+function Base.iterate(lex::Lex)
+    if length(lex.source) == 0
+        return nothing
+    end
+    res = tryparse_pos(parseToken, lex.source)
+    if res == nothing
+        error("Invalid token at $(lex.source)")
+    else
+        return res
+    end
+end
+
+function Base.iterate(lex::Lex, idx::Int64)
+    if idx > length(lex.source)
+        return nothing
+    end
+    res = tryparse_pos(parseToken, @view lex.source[idx:end])
+    if res == nothing
+        error("Invalid token at $(lex.source[idx:end])")
+    else
+        tok, len = res
+        return tok, len + idx - 1
+    end
+end
